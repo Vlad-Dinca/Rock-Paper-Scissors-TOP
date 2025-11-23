0882d93760a08dc1f83d3ceaba3bf7c014fdd5f2
Vlad Dincă <dincavlad01@gmail.com>
2025-11-23 14:48:50 +0200

    Finish the rest of the game for now

--- javascript.js
+++ javascript.js
@@ -1,65 +1,97 @@
 console.log("Hello World!");
-
+let humanScore = 0;
+let computerScore = 0;
+let gameStop = true;
 
 
 function getHumanChoice(choice){
-    return choice =parseInt(prompt("ROCK = 0, PAPER = 1, SCISSORS = 2: "));
+    return choice =parseInt(prompt("ROCK = 0, PAPER = 1, SCISSORS = 2, STOP = 3: "));
 }
 function getComputerChoice(choice){
     return choice = Math.floor(Math.random()*3);
 }
 
-let computerChoice;
-let humanChoice;
+function playRound(human, computer){
+    if (human === computer)
+            console.log("IT'S A DRAW!");
+        else if(human === 0 && computer === 1){
+            console.log("YOU LOST!");
+            computerScore=++computerScore;
+        }
+        
+        else if (human === 0 && computer === 2){
+            console.log("YOU WON!");
+            humanScore=++humanScore;
+        }
+            
+        else if (human === 1 && computer === 0){
+            console.log("YOU WON!");
+            humanScore=++humanScore;
+        }
+            
+        else if(human === 1 && computer === 2){
+            console.log("YOU LOST!");
+            computerScore=++computerScore;
+        }
+            
+        else if(human === 2 && computer === 0){
+            console.log("YOU LOST!");
+            computerScore=++computerScore;
+        }
+            
+        else if (human === 2 && computer === 1){
+            console.log("YOU WON!");
+            humanScore=++humanScore; 
+        }
 
-let computer =getComputerChoice(computerChoice);
-let human = getHumanChoice(humanChoice);
+}
 
+while(gameStop){
+    let computerChoice;
+    let humanChoice;
 
-while (human != 0 && human != 1 && human !=2){
-    console.log("You should only input 0, 1 or 2");
-    human = getHumanChoice(humanChoice);
-    }
+    let computer =getComputerChoice(computerChoice);
+    let human = getHumanChoice(humanChoice);
+    
+    while (human != 0 && human != 1 && human !=2 && human !=3){
+        console.log("You should only input 0, 1, 2 or 3");
+        human = getHumanChoice(humanChoice);
+        }
 
-if (human === computer)
-        console.log("IT'S A DRAW!");
-    else if(human === 0 && computer === 1)
-    console.log("YOU LOST!");
-    else if (human === 0 && computer === 2)
-        console.log("YOU WON!");
-    else if (human === 1 && computer === 0)
-        console.log("YOU WON!");
-    else if(human === 1 && computer === 2)
-        console.log("YOU LOST!");
-    else if(human === 2 && computer === 0)
-        console.log("YOU LOST!");
-    else if (human === 2 && computer === 1)
-        console.log("YOU WON!"); 
-
-
-
-switch(human){
-    case 0:
-        console.log("You chose Rock");
-        break;
-    case 1:
-        console.log("You chose Paper");
-        break;
-    case 2:
-        console.log("You chose Scissors");
-        break;
-   }
-
-if (human == 0 || human == 1 || human ==2)
-    switch(computer){
+    playRound(human, computer);  
+            
+    switch(human){
         case 0:
-            console.log("The computer chose Rock");
+            console.log("You chose Rock");
             break;
         case 1:
-            console.log("The computer chose Paper");
+            console.log("You chose Paper");
             break;
         case 2:
-            console.log("The computer chose Scissors");
+            console.log("You chose Scissors");
+            break;
+        case 3:
+            console.log("Game over!");
+            gameStop = false;
             break;
     }
 
+    if (human == 0 || human == 1 || human ==2)
+        switch(computer){
+            case 0:
+                console.log("The computer chose Rock");
+                break;
+            case 1:
+                console.log("The computer chose Paper");
+                break;
+            case 2:
+                console.log("The computer chose Scissors");
+                break;
+        }
+
+        if (human===3)
+            console.log(`Final score: Human = ${humanScore} Computer = ${computerScore}`)
+
+        else console.log(`Current score: Human = ${humanScore} Computer = ${computerScore}`);
+
+}
