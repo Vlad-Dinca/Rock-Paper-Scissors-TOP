67622dcf09583a37622df180659fa7c9fcc01fae
Vlad Dincă <dincavlad01@gmail.com>
2025-11-25 15:00:58 +0200

    Add style and structure to page

--- javascript.js
+++ javascript.js
@@ -1,11 +1,7 @@
 let humanScore = 0;
 let computerScore = 0;
-// let gameStop = true;
+let drawScore = 0;
 
-
-// function getHumanChoice(choice){
-//     if
-// }
 function getComputerChoice(choice){
     choice = Math.floor(Math.random()*3);
     if (choice === 0)
@@ -18,17 +14,19 @@ function getComputerChoice(choice){
 function playRound(human, computer){
     const choiceH = document.querySelector("li.hChoice");
     choiceH.textContent = `The human player chose ${human}.`;
+
     const choiceC = document.querySelector("li.cChoice");
     choiceC.textContent = `The Computer chose ${computer}.`;
-    const results = document.querySelector("li.results"); // 0 = ROCK, 1 = PAPER, 2 = SCISSORS
-    // game.appendChild("results");
-    // results.classList.add("results");
+    const results = document.querySelector("li.results");
+
     console.log(human);
-    if (human === computer)
+    if (human === computer){
             results.textContent = ("IT'S A DRAW!");
+            drawScore++;
+    }
         else if(human === "rock" && computer === "paper"){
-            results.textContent = ("YOU LOST!");
-            computerScore=++computerScore;
+        results.textContent = ("YOU LOST!");
+        computerScore=++computerScore;
         }
         
         else if (human === "rock" && computer === "scissors"){
@@ -55,22 +53,14 @@ function playRound(human, computer){
             results.textContent = ("YOU WON!");
             humanScore=++humanScore; 
         }
+    const score = document.querySelector("li.score")
+    score.textContent = `The score is: Human: ${humanScore} Computer: ${computerScore} Draws: ${drawScore}`;
 
 }
 
     let computerChoice;
     let computer;
-    // let humanChoice;
-
-  
-    // let human = getHumanChoice(humanChoice);
-    // let human;
-    
-    // while (human != 0 && human != 1 && human !=2 && human !=3){
-    //     console.log("You should only input 0, 1, 2 or 3");
-    //     human = getHumanChoice(humanChoice);
-    //     }
-
+   
     const rock = document.getElementById("rock");
     const paper = document.getElementById("paper");
     const scissors = document.getElementById("scissors");
@@ -90,38 +80,6 @@ function playRound(human, computer){
         event.preventDefault();
         playRound("scissors", computer);
     });
-    // switch(human){
-    //     case 0:
-    //         console.log("You chose Rock");
-    //         break;
-    //     case 1:
-    //         console.log("You chose Paper");
-    //         break;
-    //     case 2:
-    //         console.log("You chose Scissors");
-    //         break;
-    //     case 3:
-    //         console.log("Game over!");
-    //         gameStop = false;
-    //         break;
-    // }
-
-    // if (human == 0 || human == 1 || human ==2)
-    //     switch(computer){
-    //         case 0:
-    //             console.log("The computer chose Rock");
-    //             break;
-    //         case 1:
-    //             console.log("The computer chose Paper");
-    //             break;
-    //         case 2:
-    //             console.log("The computer chose Scissors");
-    //             break;
-    //     }
-
-    //     if (human===3)
-    //         console.log(`Final score: Human = ${humanScore} Computer = ${computerScore}`)
-
-    //     else console.log(`Current score: Human = ${humanScore} Computer = ${computerScore}`);
+    
 
 
