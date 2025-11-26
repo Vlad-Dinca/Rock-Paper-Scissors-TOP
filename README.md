3e053fd871bef8b8fb75a99adc4d865ae668a0f9
Vlad Dincă <dincavlad01@gmail.com>
2025-11-26 11:03:32 +0200

    Finish features, further improvements later

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Rock-Paper-Scissors-TOP
+
+<p dir="auto">All emojis designed by <a href="https://openmoji.org/" rel="nofollow">OpenMoji</a> – the open-source emoji and icon project. License: <a href="https://creativecommons.org/licenses/by-sa/4.0/#" rel="nofollow">CC BY-SA 4.0</a></p>
--- javascript.js
+++ javascript.js
@@ -1,6 +1,5 @@
 let humanScore = 0;
 let computerScore = 0;
-let drawScore = 0;
 
 function getComputerChoice(choice){
     choice = Math.floor(Math.random()*3);
@@ -8,7 +7,8 @@ function getComputerChoice(choice){
         return "rock";
     else if (choice === 1)
         return "paper";
-    else return "scissors"; 
+    else 
+        return "scissors"; 
 }
 
 function playRound(human, computer){
@@ -19,12 +19,11 @@ function playRound(human, computer){
     choiceC.textContent = `The Computer chose ${computer}.`;
     const results = document.querySelector("li.results");
 
-    console.log(human);
     if (human === computer){
             results.textContent = ("IT'S A DRAW!");
-            drawScore++;
     }
-        else if(human === "rock" && computer === "paper"){
+
+    if(human === "rock" && computer === "paper"){
         results.textContent = ("YOU LOST!");
         computerScore=++computerScore;
         }
@@ -53,33 +52,72 @@ function playRound(human, computer){
             results.textContent = ("YOU WON!");
             humanScore=++humanScore; 
         }
-    const score = document.querySelector("li.score")
-    score.textContent = `The score is: Human: ${humanScore} Computer: ${computerScore} Draws: ${drawScore}`;
 
+    const score = document.querySelector("li.score")
+    score.textContent = `The score is: Human: ${humanScore} Computer: ${computerScore}`;
 }
 
-    let computerChoice;
-    let computer;
-   
-    const rock = document.getElementById("rock");
-    const paper = document.getElementById("paper");
-    const scissors = document.getElementById("scissors");
+function checkScore(hScore, cScore){
+    const modal = document.getElementById("myModal");
+    const span = document.getElementsByClassName("close")[0];
+    const para = document.getElementsByClassName("modalText")[0];
+    const image = document.getElementById("modalImg");
 
-    rock.addEventListener("click", (event) =>{
-        computer = getComputerChoice(computerChoice);
-        event.preventDefault();
-        playRound("rock", computer);
-    });
-    paper.addEventListener("click", (event) =>{
-        computer = getComputerChoice(computerChoice);
-        event.preventDefault();
-        playRound("paper", computer);
-    });
-    scissors.addEventListener("click", (event) =>{
-        computer = getComputerChoice(computerChoice);
-        event.preventDefault();
-        playRound("scissors", computer);
-    });
+     if (hScore === 5){
+        modal.style.display="block";
+        para.textContent = "YOU WON!";
+        para.style="color:greenyellow";
+        image.src="./images/trophyWin.svg";
+        span.addEventListener("click", (event) =>{
+            event.preventDefault();
+            modal.style.display="none";
+        });_
+        humanScore = 0;
+        computerScore = 0;
+    }
+    else if(cScore === 5){
+        modal.style.display="block";
+        para.textContent = "YOU LOST!";
+        image.src="./images/trophyLose.svg";
+        para.style="color: red";
+        span.addEventListener("click", (event) =>{
+            event.preventDefault();
+            modal.style.display="none";
+        });_ 
+        humanScore = 0;
+        computerScore = 0;
+    }
+}
+
+let computerChoice;
+let computer;
+
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+
+rock.addEventListener("click", (event) =>{
+    computer = getComputerChoice(computerChoice);
+    event.preventDefault();
+    playRound("rock", computer);
+    checkScore(humanScore, computerScore);
+});
+paper.addEventListener("click", (event) =>{
+    computer = getComputerChoice(computerChoice);
+    event.preventDefault();
+    playRound("paper", computer);
+    checkScore(humanScore, computerScore);
+});
+scissors.addEventListener("click", (event) =>{
+    computer = getComputerChoice(computerChoice);
+    event.preventDefault();
+    playRound("scissors", computer);
+    checkScore(humanScore, computerScore);
+});
+
+const copyright = document.getElementById("copyright");
+copyright.textContent = `Copyright ©  ${(new Date().getFullYear())} Vlad-Dinca`;
+
     
 
 
